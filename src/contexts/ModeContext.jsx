6ef51d775233ca9ba4d35a9b702e3b94de9aafe9
jpepp88 <jacobeppert37@gmail.com>
2025-03-03 17:11:05 -0500

    added authentication

---
+++ src/contexts/ModeContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const ModeContext = createContext();
+
+export const ModeProvider = ({ children }) => {
+      //Variable to store the mode state
+      const [mode, setMode] = useState("light");
+      //function to update the mode state
+      const handleModeChange = () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      };
+
+      return (
+        <ModeContext.Provider value={{ mode, handleModeChange }}>
+          {children}
+        </ModeContext.Provider>
+      )
+
+};
+export default ModeContext;
--- src/pages/ProfileDetailPage.jsx
+++ src/pages/ProfileDetailPage.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Wrapper from "../components/Wrapper";
 import styles from "../styles/profiledetail.module.css";
 import { Link } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
 
 const ProfileDetailPage = () => {
   const [profile, setProfile] = useState({});
   const { id } = useParams();
+  const { isLogin } = useContext(AuthContext);
   useEffect(() => {
     fetch(
-      `https://web.ics.purdue.edu/~jeppert/profile-app/fetch-data-with-id.php?id=${id}`
+      `https://web.ics.purdue.edu/~zong6/profile-app/fetch-data-with-id.php?id=${id}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -27,7 +29,7 @@ const ProfileDetailPage = () => {
         </p>
         <p>{profile.bio}</p>
         <img src={profile.image_url} alt={profile.name} />
-        <Link to="edit" className={styles['button']}>Edit Profile</Link>
+        {isLogin && <Link to="edit" className={styles['button']}>Edit Profile</Link>}
       </div>
     </Wrapper>
   );
